5b4f09609dce2b92415471c1767575e895d5719d
pathus <panienkaczuprynienka@gmail.com>
2023-11-05 13:16:02 +0100

    restore status of tests

--- tests/smoke/webpage.spec.ts
+++ tests/smoke/webpage.spec.ts
@@ -1,30 +1,25 @@
 import { test, expect } from '@playwright/test';
-// import { HomePage } from '../../src/pages/home.page';
-// import { ArticlesPage } from '../../src/pages/articles.page';
-// import { CommentsPage } from '../../src/pages/comments.page';
+import { HomePage } from '../../src/pages/home.page';
+import { ArticlesPage } from '../../src/pages/articles.page';
+import { CommentsPage } from '../../src/pages/comments.page';
 
-// test('home page title @GAD-R01-01', async ({ page }) => {
-//   const homePage = new HomePage(page);
-//   await homePage.goto();
-//   const title = await homePage.title();
-//   expect(title).toContain('GAD');
-// });
+test('home page title @GAD-R01-01', async ({ page }) => {
+  const homePage = new HomePage(page);
+  await homePage.goto();
+  const title = await homePage.title();
+  expect(title).toContain('GAD');
+});
 
-// test('articles page title @GAD-R01-02', async ({ page }) => {
-//   const articlesPage = new ArticlesPage(page);
-//   await articlesPage.goto();
-//   const articlesTitle = await articlesPage.title();
-//   expect(articlesTitle).toContain('Articles');
-// });
+test('articles page title @GAD-R01-02', async ({ page }) => {
+  const articlesPage = new ArticlesPage(page);
+  await articlesPage.goto();
+  const articlesTitle = await articlesPage.title();
+  expect(articlesTitle).toContain('Articles');
+});
 
-// test('comments page title @GAD-R01-02', async ({ page }) => {
-//   const commentsPage = new CommentsPage(page);
-//   await commentsPage.goto();
-//   const commentsTitle = await commentsPage.title();
-//   expect(commentsTitle).toContain('Comments');
-// });
-
-test('dummy', async ({}) => {
-  const result = 2 + 4;
-  expect(result).toBeLessThanOrEqual(6);
+test('comments page title @GAD-R01-02', async ({ page }) => {
+  const commentsPage = new CommentsPage(page);
+  await commentsPage.goto();
+  const commentsTitle = await commentsPage.title();
+  expect(commentsTitle).toContain('Comments');
 });
