b3742d47685633a4d381e2ea5746f92df9758b9d
pathus <panienkaczuprynienka@gmail.com>
2023-11-05 10:55:55 +0100

    add dummy and skip test

--- tests/smoke/webpage.spec.ts
+++ tests/smoke/webpage.spec.ts
@@ -1,28 +1,28 @@
 import { test, expect } from '@playwright/test';
-import { HomePage } from '../../src/pages/home.page';
-import { ArticlesPage } from '../../src/pages/articles.page';
-import { CommentsPage } from '../../src/pages/comments.page';
+// import { HomePage } from '../../src/pages/home.page';
+// import { ArticlesPage } from '../../src/pages/articles.page';
+// import { CommentsPage } from '../../src/pages/comments.page';
 
-test.skip('home page title @GAD-R01-01', async ({ page }) => {
-  const homePage = new HomePage(page);
-  await homePage.goto();
-  const title = await homePage.title();
-  expect(title).toContain('GAD');
-});
+// test('home page title @GAD-R01-01', async ({ page }) => {
+//   const homePage = new HomePage(page);
+//   await homePage.goto();
+//   const title = await homePage.title();
+//   expect(title).toContain('GAD');
+// });
 
-test.skip('articles page title @GAD-R01-02', async ({ page }) => {
-  const articlesPage = new ArticlesPage(page);
-  await articlesPage.goto();
-  const articlesTitle = await articlesPage.title();
-  expect(articlesTitle).toContain('Articles');
-});
+// test('articles page title @GAD-R01-02', async ({ page }) => {
+//   const articlesPage = new ArticlesPage(page);
+//   await articlesPage.goto();
+//   const articlesTitle = await articlesPage.title();
+//   expect(articlesTitle).toContain('Articles');
+// });
 
-test.skip('comments page title @GAD-R01-02', async ({ page }) => {
-  const commentsPage = new CommentsPage(page);
-  await commentsPage.goto();
-  const commentsTitle = await commentsPage.title();
-  expect(commentsTitle).toContain('Comments');
-});
+// test('comments page title @GAD-R01-02', async ({ page }) => {
+//   const commentsPage = new CommentsPage(page);
+//   await commentsPage.goto();
+//   const commentsTitle = await commentsPage.title();
+//   expect(commentsTitle).toContain('Comments');
+// });
 
 test('dummy', async ({}) => {
   const result = 2 + 3;
