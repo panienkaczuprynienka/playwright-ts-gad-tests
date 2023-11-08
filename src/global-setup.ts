c20ab9e0054d9cdb34552a8085c620cb0f7f295c
pathus <panienkaczuprynienka@gmail.com>
2023-11-08 20:30:02 +0100

    add global setup

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 async function globalSetup(): Promise<void> {
   dotenv.config({ override: true });
- // console.log('!!! Base url: ', process.env.BASE_URL);
+  // console.log('!!! Base url: ', process.env.BASE_URL);
 }
 
 export default globalSetup;
