522f963a79676e9f6815a28264829c3c5799e692
pathus <panienkaczuprynienka@gmail.com>
2023-11-08 20:38:04 +0100

    add userdata

--- tests/smoke/test-1.spec.ts
+++ tests/smoke/test-1.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../src/pages/login.page';
 import { WelcomePage } from '../../src/pages/welcome.page';
+import { testUser1 } from '../../src/test-data/user.data';
 
 test.describe('Login test', () => {
   test('Login test 1 @Login', async ({ page }) => {
-    const login = 'Moses.Armstrong@Feest.ca';
+    const login = testUser1.userEmail;
     const password = 'test1';
 
     const loginPage = new LoginPage(page);
